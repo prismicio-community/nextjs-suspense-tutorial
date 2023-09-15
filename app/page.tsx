3e8aeb14325dcd802b31bd92bb5a882a1a12d6c7
Coner Murphy <coner@conermurphy.com>
2023-09-15 17:46:03 +0100

    feat: base app

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-import Pokemon from './components/Pokemon';
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ErrorBoundary fallback={<p>Something went wrong</p>}>
-        <Suspense fallback={<p className="text-white">Loading pokemon...</p>}>
-          <Pokemon />
-        </Suspense>
-      </ErrorBoundary>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24" />
   );
 }
